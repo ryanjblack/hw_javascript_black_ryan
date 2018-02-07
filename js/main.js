f6d6d079ae68a066f6ed1a87f9880fefb6d8eb21
Ryan Black <ryan.j.black@ufl.edu>
2018-02-06 21:30:21 -0500

    fixed presidents

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Question #5
+
+var presidents = [
+    {firstName: 'Donald', lastName: 'Trump', termLength: '4 years', party: 'Republican', yearsOfPresidency: '1'},
+    {firstName: 'Barack', lastName: 'Obama', termLength: '8 years', party: 'Democrat', yearsOfPresidency: '8'},
+    {firstName: 'George W.', lastName: 'Bush', termLength: '8 years', party: 'Republican', yearsOfPresidency: '8'},
+    {firstName: 'Bill', lastName: 'Clinton', termLength: '8 years', party: 'Democrat', yearsOfPresidency: '8'},
+    {firstName: 'George', lastName: 'H. W. Bush', termLength: '4 years', party: 'Republican', yearsOfPresidency: '8'},
+];
+
+console.log(presidents[2].firstName + ' ' + [presidents[2].lastName]);
